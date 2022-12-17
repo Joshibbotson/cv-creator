666456cbef09c39d56b6a209af33fbba0e575cea
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-12-17 17:50:40 +0000

    BUILD - updated Pinfo to have inputs

--- src/components/form/Pinfo.js
+++ src/components/form/Pinfo.js
@@ -1,16 +1,47 @@
-import { Fragment } from "react"
-
 export default function Pinfo() {
+    const removeDefaultValue = e => {
+        e.target.value = ""
+    }
+
     return (
         <div className="pinfo-container">
             <h2 className="pinfo-title">Personal Information</h2>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
-            <input type={"text"}></input>
+            <input
+                type="text"
+                id="title"
+                defaultValue="Title"
+                onClick={removeDefaultValue}
+            />
+            <input
+                type="text"
+                id="firstName"
+                defaultValue="First Name"
+                onClick={removeDefaultValue}
+            />
+            <input
+                type="text"
+                id="address"
+                defaultValue="Address"
+                onClick={removeDefaultValue}
+            />
+            <input
+                type="text"
+                id="phoneNumber"
+                defaultValue="Phone number"
+                onClick={removeDefaultValue}
+            />
+            <input
+                type="text"
+                id="email"
+                defaultValue="Email"
+                onClick={removeDefaultValue}
+            />
+            <textarea
+                type="textArea"
+                id="summary"
+                defaultValue="summary"
+                onClick={removeDefaultValue}
+            />
         </div>
     )
 }
